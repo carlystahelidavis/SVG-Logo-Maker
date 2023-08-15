7c40602589ccbe6d312ad59595fd360d64afb758
Carly Staheli <carlystaheli@carlys-mbp.lan>
2023-08-14 21:06:06 -0600

    created a shape class and a set color method

--- shapes.js
+++ shapes.js
@@ -1,5 +1,15 @@
 // Create Shape Class
+class Shape {
+  constructor(color) {
+    this.color = '';
+  }
 
+  setColor(color) {
+    this.color = color
+  }
+
+  getText(textInput, colorInput)
+}
 // Create Circle Class
 
 // Create Triangle Class
