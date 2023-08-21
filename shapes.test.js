8e084590e41ba655020ad085c1b1fa1fd623980f
Carly Staheli <carlystaheli@carlys-mbp.lan>
2023-08-20 22:31:25 -0600

    added the fixed test figures

--- shapes.test.js
+++ shapes.test.js
@@ -1,28 +1,34 @@
 const { Circle, Triangle, Square } = require("./shapes");
 
 describe("Circle", () => {
-  const newShape = new Circle();
-  let shapeColorInput = "red";
-  newShape.setColor(shapeColorInput);
-  expect(newShape.createShape()).toEqual(
-    `<circle cx="50%" cy="50%" r="150" height="50%" width="50%" fill="${shapeColorInput}"></circle>`
-  );
+  test("is created", () => {
+    const newShape = new Circle();
+    let shapeColorInput = "red";
+    newShape.setColor(shapeColorInput);
+    expect(newShape.createShape()).toEqual(
+      `<circle cx="50%" cy="50%" r="150" height="50%" width="50%" fill="${shapeColorInput}"></circle>`
+    );
+  });
 });
 
 describe("Triangle", () => {
-  const newShape = new Triangle();
-  let shapeColorInput = "red";
-  newShape.setColor(shapeColorInput);
-  expect(newShape.createShape()).toEqual(
-    `<polygon height="50%" width="50%" points="0,200 300,200 150,0" fill="${shapeColorInput}"></polygon>`
-  );
+  test("is created", () => {
+    const newShape = new Triangle();
+    let shapeColorInput = "red";
+    newShape.setColor(shapeColorInput);
+    expect(newShape.createShape()).toEqual(
+      `<polygon height="50%" width="50%" points="0,200 300,200 150,0" fill="${shapeColorInput}"></polygon>`
+    );
+  });
 });
 
 describe("Square", () => {
-  const newShape = new Square();
-  let shapeColorInput = "red";
-  newShape.setColor(shapeColorInput);
-  expect(newShape.createShape()).toEqual(
-    `<rect x="50" height="300" width="300" fill="${shapeColorInput}"></rect>`
-  );
+  test("is created", () => {
+    const newShape = new Square();
+    let shapeColorInput = "red";
+    newShape.setColor(shapeColorInput);
+    expect(newShape.createShape()).toEqual(
+      `<rect x="50" height="300" width="300" fill="${shapeColorInput}"></rect>`
+    );
+  });
 });
